ea047f0584cbc63595877a2e5264cf8d1e8cb6f8
kimjungwoo1007 <163504664+kimjungwoo1007@users.noreply.github.com>
2024-04-05 15:50:42 +0900

    Add files via upload

---
+++ 0405컴퓨터활용.py
@@ -0,0 +1,15 @@
+num= input("점수를 입력하세요")
+
+if (int(num)) >= 71 :
+    print("학점은 A입니다")
+
+elif (int(num))  >= 41 and (int(num)) <70 :
+    print("학점은 B입니다")
+
+elif (int(num)) >= 11 and (int(num)) <40 :
+    print("학점은 C입니다")
+
+elif (int(num)) <= 10 :
+    print("학점은 D입니다")
+
+
