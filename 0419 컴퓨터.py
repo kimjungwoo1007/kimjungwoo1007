d8b6d7394025d0cfd61d110d4ace88d331f88cae
kimjungwoo1007 <163504664+kimjungwoo1007@users.noreply.github.com>
2024-04-19 15:31:56 +0900

    Update 0419 컴퓨터.py

--- 0419 컴퓨터.py
+++ 0419 컴퓨터.py
@@ -1,7 +1,7 @@
 numbers = [[1,2,3], [4,5,6], [7,8,9]]
 results=[]
 for row in numbers :
-    for numbers in row:
+    for element in row:
         if numbers % 2 ==0:
             results.append(numbers)
 print("짝수:",results)
